bea641777b3cdadcb921429c294ec0a8aedd4e0b
Brandon Achu <Branachu2015@gmail.com>
2017-10-02 18:46:16 -0400

    added proactivity to projects.

--- src/assets/scripts/projects.js
+++ src/assets/scripts/projects.js
@@ -1,33 +1,40 @@
 export const projects = {
   0: {
+    image: "proactivity.png",
+    video: "proactivity.mp4",
+    name: "Proactivity",
+    stack: ['html', 'css', 'javascript', 'scss', 'react', 'redux', 'bootstrap', 'node', 'express', 'stripe', 'mongodb', 'git', 'github'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec sem non elit placerat viverra. Curabitur bibendum porttitor augue eu eleifend. Nulla consectetur ligula orci, quis rhoncus ligula porta in. Nulla vulputate quam augue, et convallis est accumsan a."
+  },
+  1: {
     image: "react-portfolio.png",
-    video: "portfolio-v1.mp4",
+    video: "react-portfolio.webm",
     name: "React Portfolio.v1",
     stack: ['html', 'css', 'javascript', 'scss', 'react', 'redux', 'semantic ui', 'rails', 'git', 'github'],    
     description: "After achieving working knowledge of Facebook's React library, I decided to redesign my previously lacking portfolio website to improve not only the functionality of the page, but also the UI and other optimizations."    
   },
-  1: {
+  2: {
     image: "szechuan-redesign.png",
     video: "",
     name: "Szechuan Redesign",
     stack: ['html', 'css', 'javascript', 'scss', 'react', 'redux', 'semantic ui', 'react highcharts', 'google-maps api', 'git', 'github'],
     description: "This is a redesign of a local chinese delivery restaurant in my area called Szechuan D'lite. I've been a long time customer of this local family business and one thing I noticed was how bland and messy their main website was. Given how well I loved their food I decided to spice up their website and possibly pitch the new design to them if they considered changing it. I tried to go minimilistic when choosing a design style and added subtle transitive animations to various elements to improve the UX flow."
   },
-  2: {
+  3: {
     image: "weather-app.png",
     video: "weather-app.mp4",
     name: "Forecast Five",
     stack: ['html', 'css', 'javascript', 'scss', 'react', 'redux', 'semantic ui', 'react highcharts', 'google-maps apis', 'git', 'github'],
     description: "This react/redux application allows the user to view a 5 day projection of the weather in any area of the U.S. Each search returns a map of the target area, as well as the temperature, humidity and pressure projections over the next 5 days. A maximum of 4 searches are viewable on the screen at any time to allow easy comparisons between multiple projections without cluttering."
   },
-  3: {
+  4: {
     image: "contactually-referrals.png",
     video: "contactually-referrals.webm",
     name: "Contactually Referrals",
     stack: ['html', 'css', 'javascript', 'less', 'react', 'redux', 'rails', 'semantic ui', 'amazon web-services', 'elastic search', 'fullstory', 'git', 'github'],
     description: "This was a feature of the web application that I worked on with another intern during my summer (2017) internship at Contactually. Contactually's flagship product is a CRM tool that aids users in fostering, growing and managing personal and professional relationships. This page of the application one part of our revamp of their referral system. We first converted the page's codebase from Backbone.js -> React-Redux, which improved scalability and speed. Then we implemented new features such as referral modals to improve visibility of the referral program with the goal of generating new leads for the product."
   },
-  4: {
+  5: {
     image: "random-quote.png",
     video: "random-quote.mp4",
     name: "Random Quote",
